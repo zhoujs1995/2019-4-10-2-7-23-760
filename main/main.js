cdae405b2e8919a2b02d0f92f0189ec3a1c46a1c
zhoujs1995 <13212289242@qq.com>
2019-05-26 20:24:32 +0800

    编写main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,9 @@
-module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+module.exports = function main({distance, parkTime}) {
+  // write your code here...
+  return Math.round(
+    parkTime * 0.25 +
+      6 +
+      Math.max(0, (distance - 2) * 0.8) +
+      Math.max(0, (distance - 8) * 0.4)
+  );
 };
